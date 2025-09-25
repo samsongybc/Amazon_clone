d02d7a8b72ee546edb893487925b55e313835273
samsongybc <samsongybc@gmail.com>
2025-09-24 20:58:33 -0400

    CaouselEffect modify

--- amazon_introduction/src/Components/Carousel/CarouselEffect.jsx
+++ amazon_introduction/src/Components/Carousel/CarouselEffect.jsx
@@ -2,15 +2,7 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import classes from "./Carousel.module.css";
-
-// Import images directly to avoid path issues
-const img = [
-  "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg",
-  "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Toys_en_US_1x._CB431858161_.jpg",
-  "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2020/PrimeDay/Fuji_Tallhero_PrimeDay_en_US_1x._CB418569269_.jpg",
-  "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Beauty_v2_en_US_1x._CB429089975_.jpg",
-  "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Sports_1x._CB431860448_.jpg",
-];
+import { img } from "./Img/Images/img/data";
 
 const CarouselEffect = () => {
   return (
